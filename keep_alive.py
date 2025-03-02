cb9215de809d2bccdf7e38a900f868a69349fd3a
Nallamilli Sri Ranga Sai <143283610+nallamillisrirangasai@users.noreply.github.com>
2025-03-02 12:16:37 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,7 +1,7 @@
 import time
 import requests
 
-BOT_URL = "https://your-bot-url.onrender.com"  # Replace with your Render bot URL
+BOT_URL = "https://bulk-thumb-l5mv.onrender.com"  # Replace with your Render bot URL
 
 while True:
     try:
